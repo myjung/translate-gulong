e5fe9f9f71e3e09270e0d4359e8155d63660d089
Rey <hexagramxix@gmail.com>
2024-07-03 17:58:00 +0900

    번역기 업데이트

--- gulongpatcher/translator.py
+++ gulongpatcher/translator.py
@@ -11,10 +11,32 @@ import aiohttp
 import anthropic
 
 class FreePapagoTranslator:
-    def __init__(self):
+    def __init__(self, src_lang: str = "zh-TW", target_lang: str = "ko"):
+        """
+        한국어(ko)
+        일본어(ja)
+        중국어 간체(zh-CN)
+        중국어 번체(zh-TW)
+        힌디어(hi)
+        영어(en)
+        스페인어(es)
+        프랑스어(fr)
+        독일어(de)
+        포루트갈어(pt)
+        베트남어(vi)
+        인도네시아어(id)
+        페르시아어(fa)
+        아랍어(ar)
+        미얀마어(mm)
+        태국어(th)
+        러시아어(ru)
+        이탈리아어(it)
+        """
         self.uuid = str(uuid4())
         self.url = "https://papago.naver.com/apis/n2mt/translate"
         self.version = self.setup_version()  # autmoatically set the version
+        self.src_lang = src_lang
+        self.target_lang = target_lang
 
     def setup_version(self):
         # Extract Javascript URL from Papago Page
@@ -72,8 +94,8 @@ class FreePapagoTranslator:
             "honorific": "true",
             "instant": "false",
             "paging": "false",
-            "source": "zh-TW",
-            "target": "ko",
+            "source": self.src_lang,
+            "target": self.target_lang,
             "text": text,
         }
 
